a0a49000f0bb28a73389b3f51b43c1cdefc4b5e4
Johan Kellén <johan.kellen@utb.linkoping.se>
2024-01-30 08:16:48 +0000

    igen då

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
+const Product = require('./models/Products.js');
 
-mongoose.connect("mongodb://127.0.0.1:27017/local");
+mongoose.connect("mongodb://127.0.0.1:27017/prov");
 mongoose.connection.on("connected", () => {
   console.log("MongoDB local connected");
 });
+
+
+const demo = async () => {
+  let demodata = await Product.find();
+  console.log(demodata);
+};
+demo();
+
+
